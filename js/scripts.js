521c4de44268ed7664ba90d18a5f986f4ec191f3
Andrew Fisher and Matthew Brandenburg <andrew@>
2016-08-10 15:59:05 -0700

    added final styling and script

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,4 @@
 $(document).ready(function(){
-  $("button#fake").click(function(){
-    window.open("https://www.youtube.com/watch?v=VzuDnbjIhbg");
-    $(this).hide();
-    $("#real").show();
-
-  })
   $("form#yolo").submit(function(event){
     event.preventDefault();
     var nameInput = $("input#name").val();
@@ -12,14 +6,11 @@ $(document).ready(function(){
     var locationInput = $("input:radio[name=location]:checked").val();
     var foodInput = $("#food").val();
     var colorInput = $("input#color").val();
-
+//split up DOB
     var year = birthInput.substring(0,4);
     var month = birthInput.substring(5,7);
     var day = birthInput.substring(8);
-
-
-
-
+//This is what prints in the spans in the hidden text
     $("#nameres").text(nameInput);
     $("#birthres").text(month +"/" + day+"/" +year);
     $("#placeres").text(locationInput);
@@ -27,5 +18,10 @@ $(document).ready(function(){
     $("body").css("background-color", colorInput);
     $("#result").show();
   });
-
+//not a fake button
+  $("button#fake").click(function(){
+    window.open("https://www.youtube.com/watch?v=VzuDnbjIhbg");
+    $(this).hide();
+    $("#real").show();
+  });
 });
